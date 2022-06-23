c59dd65614f3331eea99477323633307498a749f
meadow <rainbowpup26@gmail.com>
2022-06-23 09:26:02 -0400

    code for driveforward10

--- driveForward10.py
+++ driveForward10.py
@@ -1,7 +1,20 @@
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
+first = True
 def main(robot: libhousy.robot):
-    #Here is where your recurring code will go
-    print("Hello World!")
+    if first:
+        robot.lDriveEncoder.Reset()
+        robot.rDriveEncoder.Reset()
+        first=False
+
+    robot.rDrive.Set(0.8)
+    robot.lDrive.Set(0.8)
+
+    if robot.rDriveEncoder.Get() > 120:
+        robot.rDrive.Set(0)
+
+    if robot.lDriveEncoder.Get() > 120:
+        robot.rDrive.Set(0)
+
     # This tells the robot we're done and it can move on
     return libhousy.DONE
